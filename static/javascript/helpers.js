18846986dc24179e4acdcd411a86d3dd6168d741
Surribas1980 <issupl@gmail.com>
2023-09-23 20:06:17 +0000

    modificando o estilo interno da lista en móvil

--- static/javascript/helpers.js
+++ static/javascript/helpers.js
@@ -1,3 +1,5 @@
+
+
 const deletedWithQuerySelector = (atributo)=>{
   document.querySelector(`[${atributo}]`).remove();
 }
@@ -6,9 +8,17 @@ const insertEventVerListInMovil = (datosUsers)=>{
             for(let contador = 0; contador < num - 1; contador ++){
               
               document.querySelectorAll('[ver-usuario]')[contador].addEventListener('click',(event)=>{
+              
                 const ref = document.getElementsByName(`info-aux-${contador+1}`)[0];
+               
                 (event.target.style.transform == "rotate(0deg)") ?  event.target.style.transform = "rotate(90deg)" : event.target.style.transform = "rotate(0deg)";
-                (ref.style.display === 'none') ? ref.style.display = 'block' : ref.style.display = 'none';
+                if(ref.style.display === 'none'){
+                  ref.style.display = 'flex';
+                  ref.style.border = '1px solid white';
+                  ref.style.margin = '4px';
+                }else{
+                  ref.style.display = 'none';
+                }
               })
             }
 }
